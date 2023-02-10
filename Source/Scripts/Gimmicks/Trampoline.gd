80642963d22bafa434b3fad8080c03301a5d6015
DimensionWarped <dimensionwarped@gmail.com>
2023-02-09 23:58:31 -0600

    Interpolation Modes
    Added interpolation mode - center_parabolic in addition to the default linear mode that was already in use.
    There's another interpolation mode (edge parabolic) coming eventually when I unscramble my brain.
    Added the ability to disable rings all together
    Minor adjustment to default jump pushback.
    Jump pushback made exportable
    Minor adjustment to some magic numbers.

--- Source/Scripts/Gimmicks/Trampoline.gd
+++ Source/Scripts/Gimmicks/Trampoline.gd
@@ -8,17 +8,25 @@ var trampolineYPosition = 0
 
 export var platformSprite = preload("res://Graphics/Gimmicks/ICZTrampoline.png")
 export var ringsSprite = preload("res://Graphics/Gimmicks/ICZTrampolineRing.png")
-export var ringsPerSide = 3 # How many rings to draw on each side of the platform
+export var ringsPerSide = 3 # How many rings to draw on each side of the platform - set to two or less to just get rid of them.
 export var ringsMargin = 30 # Pixels away from the center of the platform to start drawing rings
 export var ringsBetween = 12 # Pixels between each ring drawn
 export var weightFactor = 10 # How many pixels the weight of one character moves the pivot point
-export var springConstant = 30.0 # No idea on what this should be just yet.
+export var springConstant = 30.0 # Somewhere between 20-30 seem like the sweet spot for trampolines.
 export var dampingFactorWeightless = 0.97 # Lower values make the trampoline slow down more quickly while no players are on it
 export var dampingFactorWeighted = 0.98 # Lower values make the trampoline slow down more quickly while one or more players are on it
-export var maxVelocity = 700 # The maximum velocity the trampoline can move at once -- acts as a limiter for how high the gimmick can launch you
-export var minVelocityForLaunch = 150 # Minimum upward velocity the trampoline must has as it is coming to the pivot in order to launch the player
+export var maxVelocity = 700 # The maximum velocity the trampoline can move at once -- acts as a soft limiter for how high the gimmick can launch you
+export var minVelocityForLaunch = 150 # Minimum upward velocity the trampoline must has as it is coming to the pivot in order to launch the player. Set it really high to disable launch behavior.
 export var bounceFactor = 1.75 # Multiplier against yVelocity for setting the player's upward launch speed
 export var baseWeight = 0 # Use this to make the trampoline itself have weight
+export var jumpPushback = 225 # This affects how much the trampoline gets pushed if a player jumps off of it.
+
+# How the rings are positioned
+# linear - the rings are placed in a line. Plain, but true to the form of the original. Works best with spaced out rings.
+# center_parabolice - the center platform is the apex/trough of the parabola of rings for that classy look
+# edge_parabolic - Each edge is the apex/trough of a parabola terminating at the platform for that divide by zero look.
+enum INTERPOLATION_MODE {linear, center_parabolic, edge_parabolic}
+export(INTERPOLATION_MODE) var interpolationMode = INTERPOLATION_MODE.linear
 
 var trampolineRealY
 var launchEnabled
@@ -69,8 +77,7 @@ func physics_process_game(delta):
 	pass
 	
 	var passedMidpoint = false
-	if (trampolineRealY < (0.66 * pivot) and yVelocity < -minVelocityForLaunch):
-		#print("yVelocity = ", yVelocity)
+	if (trampolineRealY < (0.8 * pivot) and yVelocity < -minVelocityForLaunch):
 		passedMidpoint = true
 	
 	if (launchEnabled and passedMidpoint):
@@ -98,7 +105,7 @@ func physics_process_game(delta):
 			var curVel = i.movement.y
 			i.movement.y += yVelocity
 			if (curVel < 50):
-				impart_force(150) # bounce downwards on jump
+				impart_force(jumpPushback) # bounce downwards on jump
 	pass
 	
 	# Reset player count, weight and launch to zero for next pass
@@ -117,10 +124,30 @@ func _physics_process(delta):
 		
 
 func draw_tool():
+	# Can't draw rings if there aren't enough for the anchors
+	if (ringsPerSide < 2):
+		return
+		
+	var platformOffset = weightFactor * baseWeight
 	for n in ringsPerSide:
-		draw_texture(ringsSprite, Vector2(-ringsMargin - (n * ringsBetween) - ringsSprite.get_width() / 2, 0 - (ringsSprite.get_height() / 2) + (ringsPerSide - n - 1) * (weightFactor * baseWeight) / (ringsPerSide - 1)))
-		draw_texture(ringsSprite, Vector2(ringsMargin + (n * ringsBetween)  - ringsSprite.get_width() / 2, 0 - (ringsSprite.get_height() / 2) + (ringsPerSide - n - 1) * (weightFactor * baseWeight) / (ringsPerSide - 1)))
-	draw_texture(platformSprite, Vector2(0 - platformSprite.get_width() / 2 , 0 - (platformSprite.get_height() / 2) + (weightFactor * baseWeight)))
+		var yOffset = 0
+		var spriteOffset = -(ringsSprite.get_height() / 2)
+		if interpolationMode == INTERPOLATION_MODE.linear:
+			yOffset = spriteOffset + (ringsPerSide - n - 1) * (platformOffset) / (ringsPerSide - 1)
+			pass
+		elif interpolationMode == INTERPOLATION_MODE.center_parabolic:
+			# vertex is at the platform
+			# y = a(x)^2 + k (k = platformOffset + spriteOffset)
+			yOffset = spriteOffset + platformOffset - platformOffset * pow(1.0 * n / (ringsPerSide - 1), 2)
+			pass
+		else:
+			# Sorry, it's broken.
+			pass
+			
+		draw_texture(ringsSprite, Vector2(-ringsMargin - (n * ringsBetween) - ringsSprite.get_width() / 2, yOffset))
+		draw_texture(ringsSprite, Vector2( ringsMargin + (n * ringsBetween) - ringsSprite.get_width() / 2, yOffset))
+		
+	draw_texture(platformSprite, Vector2(0 - platformSprite.get_width() / 2 , 0 - (platformSprite.get_height() / 2) + platformOffset))
 			
 	return
 
@@ -128,7 +155,28 @@ func _draw():
 	if Engine.is_editor_hint():
 		return draw_tool()
 		
+	# Can't draw rings if there aren't enough for the anchors
+	if (ringsPerSide < 2):
+		return
+		
+	var platformOffset = body.position.y
+		
 	for n in ringsPerSide:
-		draw_texture(ringsSprite, Vector2(-ringsMargin - (n * ringsBetween) - ringsSprite.get_width() / 2, 0 - (ringsSprite.get_height() / 2) + (ringsPerSide - n - 1) * body.position.y / (ringsPerSide - 1)))
-		draw_texture(ringsSprite, Vector2(ringsMargin + (n * ringsBetween)  - ringsSprite.get_width() / 2, 0 - (ringsSprite.get_height() / 2) + (ringsPerSide - n - 1) * body.position.y / (ringsPerSide - 1)))
+		var yOffset = 0
+		var spriteOffset = -(ringsSprite.get_height() / 2)
+		if interpolationMode == INTERPOLATION_MODE.linear:
+			yOffset = spriteOffset + (ringsPerSide - n - 1) * (platformOffset) / (ringsPerSide - 1)
+			pass
+		elif interpolationMode == INTERPOLATION_MODE.center_parabolic:
+			# vertex is at the platform
+			# y = a(x)^2 + k (k = platformOffset + spriteOffset)
+			yOffset = spriteOffset + platformOffset - platformOffset * pow(1.0 * n / (ringsPerSide - 1), 2)
+			pass
+		else:
+			# Sorry, it's broken.
+			pass
+			
+		draw_texture(ringsSprite, Vector2(-ringsMargin - (n * ringsBetween) - ringsSprite.get_width() / 2, yOffset))
+		draw_texture(ringsSprite, Vector2( ringsMargin + (n * ringsBetween) - ringsSprite.get_width() / 2, yOffset))
+		
 	draw_texture(platformSprite, Vector2(0 - platformSprite.get_width() / 2 , 0 - (platformSprite.get_height() / 2) + body.position.y))
