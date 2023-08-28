33595ca010e4f903c602b2b59466c27126a10b90
Renhoex <cablurton@hotmail.com>
2023-08-28 19:35:09 +0800

    platform fix, set renderer to compatibility
    - added check to platform to prevent crashes
    - set renderer to compatibility mode

--- Scripts/Objects/PlatformChild.gd
+++ Scripts/Objects/PlatformChild.gd
@@ -3,4 +3,5 @@ extends AnimatableBody2D
 # drop condition
 func physics_collision(body, hitVector):
 	if hitVector.y > 0 and body.ground:
-		get_parent().doDrop = true
+		if get_parent().get("doDrop") != null:
+			get_parent().doDrop = true
