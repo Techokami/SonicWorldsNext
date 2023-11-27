4c0411e26a3c8e2834abc7721d565b89183d2685
Blue Staggo <owengrenney@gmail.com>
2023-11-27 20:56:51 +0000

    Sprite fixes

--- Scripts/Player/ShieldAnimations.gd
+++ Scripts/Player/ShieldAnimations.gd
@@ -2,6 +2,9 @@
 extends AnimatedSprite2D
 
 
+const DEFAULT_OFFSET := Vector2(0, -1)
+
+
 func _process(_delta):
 	# prevent rotation
 	if (!Engine.is_editor_hint()):
@@ -12,14 +15,14 @@ func _on_Shields_frame_changed():
 	match (animation):
 		"Default": # regular shield
 			z_index = 6
-			offset = Vector2.ZERO
+			offset = DEFAULT_OFFSET
 			scale = Vector2(1,1)
 			
 			if (material.blend_mode != 1):#BLEND_MODE_ADD):
 				material.blend_mode = 1#BLEND_MODE_ADD
 		"Bubble":
 			z_index = 6
-			offset = Vector2.ZERO
+			offset = DEFAULT_OFFSET
 			scale.x = 1
 			# flip vertically if frame is odd and greater then 30
 			if (fmod(frame,2) == 1 and frame > 30):
@@ -33,7 +36,7 @@ func _on_Shields_frame_changed():
 				z_index = 4
 			else:
 				z_index = 6
-			offset = Vector2.ZERO
+			offset = DEFAULT_OFFSET
 			# if frames greater then 5 or 13 and less then 10 and 18, flip horizontaly
 			if (frame >= 5 and frame < 10 or
 			frame >= 13 and frame < 18):
@@ -55,12 +58,12 @@ func _on_Shields_frame_changed():
 			else:
 				scale.y = 1
 			scale.x = 1
-			offset = Vector2.ZERO
+			offset = DEFAULT_OFFSET
 			if (material.blend_mode != 0):#BLEND_MODE_DISABLED):
 				material.blend_mode = 0#BLEND_MODE_DISABLED
 		_: # default
 			z_index = 6
-			offset = Vector2.ZERO
+			offset = DEFAULT_OFFSET
 			scale = Vector2(1,1)
 			if (material.blend_mode != 0):#BLEND_MODE_DISABLED):
 				material.blend_mode = 0#BLEND_MODE_DISABLED
