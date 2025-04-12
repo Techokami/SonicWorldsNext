a66bd10ace6c3b05c15446ec97c79481ac8331f6
Daniel-Cortez <gromovstanislav@yandex.ru>
2025-04-12 21:00:47 +0700

    AltSpawn.gd: Get rid of hardcoded list of characters

--- Scripts/Misc/AltSpawn.gd
+++ Scripts/Misc/AltSpawn.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-@export_enum("Sonic","Tails","Knuckles","Amy")var currentCharacter = 0
+@export var current_character:Global.CHARACTERS = 1 as Global.CHARACTERS
 # alternative spawning location
 func _ready():
-	if currentCharacter == Global.PlayerChar1-1 and Global.currentCheckPoint == -1:
+	if current_character == Global.PlayerChar1 and Global.currentCheckPoint == -1:
 		Global.players[0].global_position = global_position
 		Global.players[0].camera.global_position = global_position
