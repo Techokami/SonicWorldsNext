6a02aa99babf838dc33bc589272507a0c4df239d
Daniel-Cortez <gromovstanislav@yandex.ru>
2025-06-02 20:58:43 +0700

    Add a custom constructor for `BreakableBlockPiece`

--- Scripts/Misc/BreakableBlockPiece.gd
+++ Scripts/Misc/BreakableBlockPiece.gd
@@ -1,8 +1,27 @@
-extends Sprite2D
+class_name BreakableBlockPiece extends Sprite2D
 
 var gravity = 0.21875
 var velocity = Vector2.ZERO
 var lifeTime = 5 # 5 seconds
+
+## Creates a BreakableBlockPiece object.[br]
+## [param parent] - parent object the piece will be attached to.[br]
+## [param global_pos] - coordinates to create the Score object at
+##                      ([b]not[/b] relative to the [param parent]'s coordinates).[br]
+## [param p_velocity] - initial velocity of the newly created piece.[br]
+## [param p_texture] - texture of the newly created piece.[br]
+## [param region_rect] - the region of the texture to display.[br]
+## [param z_idx] - Z rendering order of the newly created piece.
+static func create(parent: Node, global_pos: Vector2, p_velocity: Vector2,
+				   p_texture: Texture2D, region_rect: Rect2, z_idx: int) -> BreakableBlockPiece:
+	var piece: BreakableBlockPiece = preload("res://Entities/Misc/BlockPiece.tscn").instantiate()
+	piece.velocity = p_velocity
+	piece.texture = p_texture
+	piece.region_rect = region_rect
+	piece.z_index = z_idx
+	parent.add_child(piece)
+	piece.global_position = global_pos
+	return piece
 
 func _physics_process(delta):
 	# increase gravity
--- Scripts/Objects/BreakableBlock.gd
+++ Scripts/Objects/BreakableBlock.gd
@@ -1,12 +1,13 @@
 extends StaticBody2D
 @export var pieces = Vector2(2,2)
-var Piece = preload("res://Entities/Misc/BlockPiece.tscn")
 @export var sound = preload("res://Audio/SFX/Gimmicks/Collapse.wav")
 @export var score = true
 
 func physics_collision(body, hitVector):
 	# check if physics object is coming down and check for a bit where the player isn't on floor
 	if hitVector == Vector2.DOWN and body.get_collision_layer_value(20):
+		var parent: Node = get_parent()
+		
 		# disable collision
 		$CollisionShape2D.disabled = true
 		$Sprite2D.visible = false
@@ -16,33 +17,30 @@ func physics_collision(body, hitVector):
 		body.ground = false
 		body.movement.y = -3*60
 		if score:
-			Score.create(get_parent(), global_position, Global.SCORE_COMBO[min(Global.SCORE_COMBO.size()-1,body.enemyCounter)])
+			Score.create(parent, global_position, Global.SCORE_COMBO[min(Global.SCORE_COMBO.size()-1,body.enemyCounter)])
 		body.enemyCounter += 1
 		
-		var sprite_width: int = $Sprite2D.texture.get_width()
-		var sprite_height: int = $Sprite2D.texture.get_height()
+		var sprite_texture: Texture2D = $Sprite2D.texture
+		var sprite_width: int = sprite_texture.get_width()
+		var sprite_height: int = sprite_texture.get_height()
 		if $Sprite2D.region_enabled:
 			sprite_width = $Sprite2D.region_rect.size.x
 			sprite_height = $Sprite2D.region_rect.size.y
 		
 		# generate pieces of the block to scatter, use i and j to determine the velocity of each one
 		# and set the settings for each piece to match up with the $Sprite2D node
-		for i in range(pieces.x):
-			for j in range(pieces.y):
-				var piece = Piece.instantiate()
-				
-				piece.velocity = Vector2(
-					(pieces.y-j)*lerp(-1,1,i/(pieces.x-1)),
-					-pieces.y+j)*60
-				
-				piece.global_position = global_position+Vector2(
-					sprite_width/4*lerp(-1,1,i/(pieces.x-1)),
-					sprite_height/4*lerp(-1,1,j/(pieces.y-1)))
-				piece.texture = $Sprite2D.texture
-				piece.z_index = z_index
-				piece.region_rect = Rect2(
-					Vector2((sprite_width/pieces.x)*i,(sprite_height/pieces.y)*j),
-					Vector2(sprite_width/pieces.x,sprite_height/pieces.y))
-				get_parent().add_child(piece)
+		for i: float in range(pieces.x):
+			for j: float in range(pieces.y):
+				BreakableBlockPiece.create(
+					parent,
+					global_position+Vector2(
+						sprite_width/4*lerp(-1,1,i/(pieces.x-1)),
+						sprite_height/4*lerp(-1,1,j/(pieces.y-1))),
+					Vector2((pieces.y-j)*lerp(-1,1,i/(pieces.x-1)),-pieces.y+j)*60,
+					sprite_texture,
+					Rect2(
+						Vector2((sprite_width/pieces.x)*i,(sprite_height/pieces.y)*j),
+						Vector2(sprite_width/pieces.x,sprite_height/pieces.y)),
+					z_index)
 				
 	return true
