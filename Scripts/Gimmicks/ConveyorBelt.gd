eef19f683ab678e3e9a266e95864c4c85897bd1b
Daniel-Cortez <gromovstanislav@yandex.ru>
2026-01-03 03:24:30 +0700

    ConveyorBelt.gd: Simplify calculations in `_process()`, move duplicate code into a separate function

--- Scripts/Gimmicks/ConveyorBelt.gd
+++ Scripts/Gimmicks/ConveyorBelt.gd
@@ -5,24 +5,25 @@ extends StaticBody2D
 var frame = 0
 @export var length = 1
 
+func _set_sprite_positions() -> void:
+	$MiddlePiece.region_rect.size.x = length * 32.0
+	$LeftCog.position.x = -length * 16.0
+	$RightCog.position.x = length * 16.0
+	$CollisionShape2D.scale.x = 1.0 + length
+
 func _ready():
 	# sprite related animations
-	$MiddlePiece.region_rect.size.x = length*32
-	$LeftCog.position.x = -length*16
-	$RightCog.position.x = length*16
-	$CollisionShape2D.scale.x = length+1
+	_set_sprite_positions()
 	# constant linear velocity is a constant speed set in godot's physics
 	constant_linear_velocity.x = speed
 
 func _process(delta):
 	if Engine.is_editor_hint():
-		$MiddlePiece.region_rect.size.x = length*32
-		$LeftCog.position.x = -length*16
-		$RightCog.position.x = length*16
-		$CollisionShape2D.scale.x = length+1
+		_set_sprite_positions()
 		
 	frame = wrapf(frame+(delta*speed/2),0,3)
 	
-	$MiddlePiece.frame = (floor(frame)*3)+2
-	$LeftCog.frame = (floor(frame)*3)
-	$RightCog.frame = (floor(frame)*3)+1
+	var anim_frame: int = floori(frame)*3
+	$LeftCog.frame = anim_frame
+	$RightCog.frame = anim_frame + 1
+	$MiddlePiece.frame = anim_frame + 2
