5eb05528767f3bc67c490673a0701c8ce5c4b5cc
Caverns4 <Caverns4@users.noreply.github.com>
2026-09-13 15:03:37 -0400

    Restore deletions/changes, just include EHZ1/2 tweaks

---
+++ Scripts/PropertiesLookup.gd
@@ -0,0 +1,7 @@
+extends Node
+# This script is purely for lookup physics variables to avoid grouping
+# physics variables in with objects, 
+
+@export var speedStepLimit = 8
+@export var groundLookDistance = 14
+@export var sonic2FloorSnap = true
