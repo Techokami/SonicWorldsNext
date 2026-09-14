20592d0cff72cda938e4f4fb88463bdf47522c13
Stanislav Gromov <Daniel-Cortez@users.noreply.github.com>
2026-09-15 04:15:06 +0700

    Chomper badnik: Set `EnemyBase` as the parent node (#370)
    Merge Chopper/Masher fixes from Daniel Cortez

--- Scripts/Enemies/Chomper.gd
+++ Scripts/Enemies/Chomper.gd
@@ -1,43 +1,48 @@
 @tool
 extends EnemyBase
 
+## The height for the Chomper to jump at the first time it comes onscreen.
+## Mashers from Sonic 2 erroniously jump 1 unit lower the first time they load.
+@export var first_jump_height: float = 7.0
+## The height for the Chopper to jump at after the first time.
+@export var jump_height: float = 7.0
+## The sprite graphic to use.
+@export var sprite_image: Texture2D = preload("res://Graphics/Enemies/chomper.png")
 
-@onready var sprite = $Chomper
+@onready var _initial_pos_y: float = position.y
 
 # Original Code RefCounted translated by VAdaPEGA
 var animFrame = 0.0
-var jump_height: float = -7.0
 
 func _ready():
 	if Engine.is_editor_hint():
 		return
 	# initial velocity
-	jump_height = abs(get_parent().jump_height)
-	sprite.texture = get_parent().sprite_image
-	velocity.y = 0-jump_height
+	jump_height = -jump_height
+	$Chomper.texture = sprite_image
+	velocity.y = -first_jump_height
 	set_physics_process(false)
 	set_process(false)
 
-
 func _physics_process(delta):
 	if Engine.is_editor_hint():
-		sprite.texture = get_parent().sprite_image
+		$Chomper.texture = sprite_image
 		return
 	
 	# gravity
 	velocity.y += 0.09375/GlobalFunctions.div_by_delta(delta)
-	position.y = min(position.y,0)
+	position.y = minf(position.y, _initial_pos_y)
 	
 	# reset velocity
-	if position.y >= 0:
-		velocity.y = 0-jump_height*60
+	if position.y >= _initial_pos_y:
+		velocity.y = jump_height * 60.0
 
 func _process(delta):
 	if Engine.is_editor_hint():
 		return
 	super(delta)
 	# animation states
-	if position.y > -192:
+	if position.y > _initial_pos_y - 192.0:
 		if velocity.y > 0:
 			# stationary
 			animFrame = 0
